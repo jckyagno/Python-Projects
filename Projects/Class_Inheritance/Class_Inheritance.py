defb6fcd108f8cbc6a1ed2ef84deaba0b6690386
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-07-07 08:04:01 -1000

    Create Class_Inheritance.py

---
+++ Projects/Class_Inheritance/Class_Inheritance.py
@@ -0,0 +1,17 @@
+class vehicle:
+    brand = ''
+    year = int()
+    wheelNumber = 4
+    engineType = "internal combustion engine"
+    doorNumber = 4
+    passengerNumber = 4
+
+class Tacoma(vehicle):
+    brand = Toyota
+    year = 2023
+    bedLength = 73.2
+
+class Bolt(vehicle):
+    brand = Chevrolet
+    year = 2023
+    engineType = "electric"
